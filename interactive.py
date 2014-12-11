ba984844115b6787ff613595beffdf00c92b88e1
Victor Geislinger <VictorLoren@users.noreply.github.com>
2014-12-11 14:31:50 -0800

    Add interactive.py file   - Add an interactive script to be used in the terminal.

---
+++ interactive.py
@@ -0,0 +1,21 @@
+#Main program executor
+import pyRecipeBook
+import FoodGroups
+
+#Welcome screen
+welcomeMessage =  "Welcome to pyRecipeBook!\n"
+welcomeMessage += "Enter a command below:\n"
+print(welcomeMessage)
+
+
+#
+pre = '# '
+on = True
+
+#Keep asking for inpyt
+while(on):
+	command = input(pre)
+	#Run command
+
+#Exiting commands	
+"Thank you for choosing to use pyRecipeBook"
