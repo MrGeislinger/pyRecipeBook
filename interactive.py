5d68184fcfe12ddf8486b8a498c1438143cae452
Victor Geislinger <VictorLoren@users.noreply.github.com>
2014-12-11 16:59:43 -0800

    Update interactive.py   - Add experimental compatibility for adding items to a shopping list.

--- interactive.py
+++ interactive.py
@@ -1,19 +1,47 @@
 #Main program executor
 import pyRecipeBook
 import FoodGroups
+import ShoppingList as List
 
 #Welcome screen
 welcomeMessage =  "Welcome to pyRecipeBook!\n"
 welcomeMessage += "Enter a command below:\n"
 print(welcomeMessage)
 
+myShoppingList = []
+
 #Method to run commands
-def runCommand(command):
-	if command.strip() == 'exit':
+def runCommand(command,myShoppingList=[1]):
+	#Split command
+	args = command.split()
+	
+	#
+	if len(args) < 1: #No command -> pass
+		return True
+	elif args[0] == 'exit': #Exit command
+		print('Exiting...')
 		return False
-	else:
+	elif args[0] == 'add': #Proceed to add what is given
+		#Test whether the arguements are valid
+		if len(args) == 1: #Not enough arguements
+			print "Minimum of 2 arguements must be given"
+		elif len(args) >4:
+			print "Maximum of 4 arguements must be given"
+		else: #Add item to list
+			#Convert the arguements to something to input into Python method 
+			newArgs = (myShoppingList,) + tuple(args[1:])
+			#Add to the shopping list
+			myShoppingList = addToList(*newArgs)
+		return True
+	else: #Command isn't defined in this tool
+		print('That command doesn\'t exist.')
 		return True
 
+#Add item to shopping list
+def addToList(myList,item,quantity=1,unit=''):
+	myItem = List.Item(item,quantity,unit)
+	myList.append(myItem)
+	return myList
 #
 pre = '# '
 on = True
