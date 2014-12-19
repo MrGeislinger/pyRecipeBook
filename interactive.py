6db509db1927ff2b3a19f2d5ff5cff1e43d8abb5
Victor Loren Geislinger <VictorLoren@users.noreply.github.com>
2014-12-18 19:44:46 -0800

    Update interactive.py
      - Add method to print the shopping list.
      - Clean up some comments.

--- interactive.py
+++ interactive.py
@@ -8,29 +8,33 @@ welcomeMessage =  "Welcome to pyRecipeBook!\n"
 welcomeMessage += "Enter a command below:\n"
 print(welcomeMessage)
 
+#Initiate a shopping list
 myShoppingList = []
 
 #Method for `exit` command
 def exitCom(args):
 	#Predefined so args is never less than 1
-	#Too many arguements
-	if len(args) > 1:
+	if len(args) > 1: #Too many arguements
 		return True
 	#Interactive mode should is not on
 	return False
+
+#Method to print the shopping list
+def printShoppingList(myList):
+	print myList
 
 #Method to run commands
 def runCommand(command,myShoppingList=[]):
 	#Split command
 	args = command.split()
-	
 	#
 	if len(args) < 1: #No command -> pass
 		return True
 	elif args[0] == 'exit': #Exit command
 		print('Exiting...')
 		return exitCom(args)
-	elif args[0] == 'add': #Proceed to add what is given
+	#Proceed to add what is given
+	elif args[0] == 'add': 
 		#Test whether the arguements are valid
 		if len(args) == 1: #Not enough arguements
 			print "Minimum of 2 arguements must be given"
@@ -38,19 +42,20 @@ def runCommand(command,myShoppingList=[]):
 			print "Maximum of 4 arguements must be given"
 		else: #Add item to list
 			#Convert the arguements to something to input into Python method 
-			newArgs = (myShoppingList,) + tuple(args[1:])
+			newArgs = (myShoppingList,) + tuple(args[1:]) #list, item, (quantity), (unit)
 			#Add to the shopping list
 			myShoppingList = addToList(*newArgs)
 		return True
-	elif args[0] == 'shoppingList': #Operate on the shopping list
+	#Operate on the shopping list
+	elif args[0] == 'shoppingList': 
 		#
 		if len(args) == 1: #Print the list
-			print myShoppingList
+			printShoppingList()
 		else: #Not valid arguments
 			print "Command not valid"
 		return True
-		
-	else: #Command isn't defined in this tool
+	#Command isn't defined	
+	else: 
 		print('That command doesn\'t exist.')
 		return True
 
